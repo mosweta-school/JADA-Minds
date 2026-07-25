8d7ad9ea8a86f91272d7983778d879b7321b81c6
mosweta-school <deograciousmoriasi10@gmail.com>
2026-07-25 14:11:13 +0300

    Set up Flask backend foundation

---
+++ backend/app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+from .config import Config
+from .extensions import db, migrate, jwt
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+
+    @app.route("/")
+    def home():
+        return {
+            "status": "success",
+            "message": "Welcome to JADA Minds API 🚀"
+        }
+
+    return app
